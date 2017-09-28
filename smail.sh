1c90bf1943cadf41b6243f8d32cc79d4e719abd3
«VitaliyDanevych» <vitaliy.danevych@life.com.ua>
2017-09-28 09:55:41 +0300

    second final commit

--- smail.sh
+++ smail.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-TO="oleksiy.yevstratov@lifecell.com.ua,petr.fedyankin@lifecell.com.ua,vladislav.samborskiy@lifecell.com.ua,ext_huawei_erhan.ay@lifecell.com.ua,vitaliy.danevych@lifecell.com.ua,anastasia.gerliand@lifecell.com.ua,taras.chagayna@lifecell.com.ua"
+TO="olyeaov@lifecell.com.ua,penn@lifecell.com.ua,vladskiy@lifecell.com.ua,ext_an.ay@lifecell.com.ua,vitach@lifecell.com.ua,anaand@lifecell.com.ua,taryna@lifecell.com.ua"
 
-#CC="vitaliy.danevych@life.com.ua"
-FROM="ericsson-oss-rc2@lifecell.com.ua"
+#CC="vitach@life.com.ua"
+FROM="ericsson-o2@lifecell.com.ua"
 SUBJECT="Ericcson RBS data (licenses, e_tilt) report"
 BODY="Pls, find attached archive file. There is UNIX end of line at the txt files into it. Windows\DOS uses carriage return and line feed ("\\r\\n") as a line ending, which Unix uses just line feed ("\\n")"
-(echo 'From: '$FROM;echo 'To:'$TO;echo 'Cc:'$CC;echo 'Subject: '$SUBJECT;  echo $BODY ;uuencode $1 $1)| /usr/sbin/sendmail -f vitaliy.danevych@lifecell.com.ua -t
+(echo 'From: '$FROM;echo 'To:'$TO;echo 'Cc:'$CC;echo 'Subject: '$SUBJECT;  echo $BODY ;uuencode $1 $1)| /usr/sbin/sendmail -f vitch@lifecell.com.ua -t
 exit
 
